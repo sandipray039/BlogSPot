362fa2f3ba1d229fa5b5fbdbf8cdcb3524ed1aa3
sandipray039 <sandipray0391@gmail.com>
2025-02-09 03:04:57 +0530

    trying to solve route problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, RouterProvider,createBrowserRouter } from "react-router";
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";  // Corrected import
+import './index.css';
+import App from './App.jsx';
 import { Home } from "./pages/Home.jsx";
 import Blogs from "./pages/Blogs.jsx";
 import Contact from "./pages/Contact.jsx";
@@ -12,44 +12,26 @@ import Login from "./pages/Login.jsx";
 import OneBlog from "./pages/OneBlog.jsx";
 import SignUp from "./pages/SignUp.jsx";
 
-
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:"/", element:<App/>,
-    children:[
-      {
-        path:"/", element:<Blogs/>
-      },
-      {
-        path:"/blogs", element:<Blogs/>
-      },
-      {
-        path: "/blogs/:id", element: <OneBlog />  
-      },
-      {
-        path:"/contact", element:<Contact/>
-      },
-      {
-        path:"/services", element:<Services/>
-      },
-      {
-        path:"/about", element:<About/>
-      },
-      {
-        path:"/login", element:<Login/>
-      },
-      {
-        path:"/signup", element:<SignUp/>
-      },
-
+    path: "/", element: <App />,
+    children: [
+      { path: "/", element: <Blogs /> },
+      { path: "/blogs", element: <Blogs /> },
+      { path: "/blogs/:id", element: <OneBlog /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/services", element: <Services /> },
+      { path: "/about", element: <About /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <SignUp /> },
     ]
   }
-])
+]);
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-<React.StrictMode>
-  <RouterProvider router={router}/>
-</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
